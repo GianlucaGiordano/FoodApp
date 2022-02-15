904457003fe2e468b88ac7330431fc15ea2fd67c
unknown <gianluc.giordano@gmail.com>
2022-02-15 16:12:12 +0100

    Changed DUMMY_DATA with fetched API in AvailableMeals.js and Removede store folder

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,33 @@
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useState, useEffect } from "react";
 
 const AvailableMeals = (props) => {
-  const mapDummy = DUMMY_MEALS.map((meal) => (
+  const [foodList, setFoodlist] = useState([]);
+
+  useEffect(() => {
+    const fetchFoodHandler = async () => {
+      const response = await fetch(
+        "https://food-app-10c79-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      const data = await response.json();
+
+      const foodsData = [];
+      for (const item in data) {
+        foodsData.push({
+          id: item,
+          name: data[item].name,
+          description: data[item].description,
+          price: data[item].price,
+        });
+      }
+      setFoodlist(foodsData);
+    };
+    fetchFoodHandler();
+  }, []);
+
+  const mapDummy = foodList.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
